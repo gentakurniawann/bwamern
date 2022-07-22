f2b4887fadf6478b429e68cf5a92751aad832fab
gentakurniawann <muhammad_genta_29rpl@student.smktelkom-mlg.sch.id>
2022-07-22 16:23:00 +0700

    first commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import moduleName from 'element/Button'
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
